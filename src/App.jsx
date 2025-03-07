c4624e331481a871da313919fb31bf61e88a56d3
Ian Rivera <rian27116@gmail.com>
2025-03-07 11:47:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/header/Header'
 import UserData from './components/userData/UserData'
 import Objectives from './components/objectives/Objectives';
 import Welcome from './components/welcome/Welcome';
-import MedicalHistory from './components/MedicalHistory/MedicalHistory';
+import MedicalHistory from './components/medicalHistory/MedicalHistory';
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
